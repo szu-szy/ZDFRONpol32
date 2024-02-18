9a47524fafae9364406fc4c9043bbe282d04eefc
Szuszy <sz.szumski.ext@olx.pl>
2024-02-18 09:00:52 +0100

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens: {
+      md: "500px",
+      lg: "650px",
+    },
+    colors: {
+      // I sposob
+      porzeczkowy: "#b345ca",
+      // II sposób
+      brunatny: {
+        100: "#abc345",
+        800: "#abc456",
+      },
+    },
+    extend: {
+      fontFamily: {
+        customFont: "Anta",
+      },
+    },
   },
   plugins: [],
-}
-
+};
